d1b67f9ccfaf8a01ea96b799047164cdfadc9c7e
Chyhong32 <63396094+Chyhong32@users.noreply.github.com>
2020-04-26 16:35:54 +0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -178,106 +178,107 @@ void function_riel () //1.borey //2.Pich & Hong //3.Mr.Hao
         }
             
     void function_dollar()
-	{
-	printf(" Please input exchange rate: riel");
-	scanf("%lf",&exrate);
-	
-	printf("Enter Salary : $");
-	scanf("%lf",&salary);
-	
-	printf("Enter Bonus of salary : $");
-	scanf("%lf",&bonus);
-	
-	printf("Do you have Husband or Wife? (Y/N) \n");
-	scanf(" %c",&spouse_str);
-	
-	printf("Enter Child :");
-	scanf("%lf",&child);
-	
-	switch(spouse_str)
-	{
-		case 'y':
-			{
-				spouse =1;
-				break;
-			}
-		case 'n':
-			{
-				spouse =0;
-				break;
-			}
-		case 'Y':
-			{
-				spouse =1;
-				break;	
-			}
-		case 'N':
-			{
-				spouse =0;
-				break;
-			}
-	}	
+	  printf(" Please input exchange rate in riel :");
+       scanf("%lf",&exrate);
+       
+       printf("Enter Salary : $");
+       scanf("%lf",&salary);
+       
+       printf("Enter Bonus of salary : $");
+       scanf("%lf",&bonus);
+       
+       printf("Do you have Husband or Wife? (Yes/No) \n");
+       scanf(" %c",&spouse_str);
+       
+       printf("Enter Child :");
+       scanf("%lf",&child);
+       
+       switch(spouse_str)
+       {
+           case 'y':
+               {
+                   spouse =1;
+                   break;
+               }
+           case 'n':
+               {
+                   spouse =0;
+                   break;
+               }
+           case 'Y':
+               {
+                   spouse =1;
+                   break;
+               }
+           case 'N':
+               {
+                   spouse =0;
+                   break;
+               }
+       }
 
- if (salary >= 0 && bonus >=0)
-            {
-                if (child >= 9)
-                    child = 9;
-                else if (child <= 0)
-                    child = 0;
-                if (spouse >= 0)
-                    spouse = 1;
-                else
-                    spouse = 0;
-                    
-                family = spouse + child; //sum husband or wife and childs //family member cal
+    if (salary >= 0 && bonus >=0)
+               {
+                   if (child >= 9)
+                       child = 9;
+                   else if (child <= 0)
+                       child = 0;
+                   if (spouse >= 0)
+                       spouse = 1;
+                   else
+                       spouse = 0;
+                       
+                   family = spouse + child; //sum husband or wife and childs //family member cal
 
-                salary_notax = 150000 * family; //find salary without tax
-               // salary_notax = salary_notax*exrate;
-				suminriel= (salary+bonus)*exrate;
-                salary = suminriel - salary_notax;
+                   salary_notax = 150000 * family; //find salary without tax
+                  // salary_notax = salary_notax*exrate;
+                   suminriel= (salary+bonus)*exrate;
+                   salary = suminriel - salary_notax;
 
-                if (salary >= 0 && salary <= 1300000)
-                {
-                    tax = 0.0;
-                    salary_tax = salary * tax;
-                    //printf("Tax rate : %lf",tax);
-                }
-                else if (salary >= 1300001 && salary <= 2000000)
-                {
-                    tax = 0.05;
-                    salary_tax = ((salary * tax) - 65000);
-                    //printf("Tax rate : %lf",tax);
-                }
-                else if (salary >= 2000001 && salary <= 8500000)
-                {
-                    tax = 0.10;
-                    salary_tax = (salary * tax) - 165000;
-                    //printf("Tax rate : %lf",tax);
-                }
-                else if (salary >= 8500001 && salary <= 12500000)
-                {
-                    tax = 0.15;
-                    salary_tax = (salary * tax) - 590000;
-                    //printf("Tax rate : %lf",tax);
-                }
-                else if (salary > 12500000)
-                {
-                    tax = 0.20;
-                    salary_tax = (salary * tax) - 1215000;
-                    //printf("Tax rate : %lf",tax);  
-                }
-                salary_tax += bonus * 0.2;
-				
-				printf("\n");
-              	printf("=================\n");
-              	printf("Tax rate : %lf %\n",tax);  
-                printf("Tax Of salary : %lfRiels\n", salary_tax);
-               printf("Tax Of salary : %lf$\n",salary_tax*exrate);
+                   if (salary >= 0 && salary <= 1300000)
+                   {
+                       tax = 0.0;
+                       salary_tax = salary * tax;
+                       //printf("Tax rate : %lf",tax);
+                   }
+                   else if (salary >= 1300001 && salary <= 2000000)
+                   {
+                       tax = 0.05;
+                       salary_tax = ((salary * tax) - 65000);
+                       //printf("Tax rate : %lf",tax);
+                   }
+                   else if (salary >= 2000001 && salary <= 8500000)
+                   {
+                       tax = 0.10;
+                       salary_tax = (salary * tax) - 165000;
+                       //printf("Tax rate : %lf",tax);
+                   }
+                   else if (salary >= 8500001 && salary <= 12500000)
+                   {
+                       tax = 0.15;
+                       salary_tax = (salary * tax) - 590000;
+                       //printf("Tax rate : %lf",tax);
+                   }
+                   else if (salary > 12500000)
+                   {
+                       tax = 0.20;
+                       salary_tax = (salary * tax) - 1215000;
+                       //printf("Tax rate : %lf",tax);
+                   }
+                   salary_tax += bonus * 0.2;
+                   
+                   printf("\n");
+                   printf("==========================================================\n");
+                   printf("= Tax rate           : %lf % \n",tax);
+                   printf("= Tax Of salary      : %lfRiels \n", salary_tax);
+                  printf("= Tax Of salary      : %lf$ \n",salary_tax*exrate);
 
-                last_salary = (salary + salary_notax + bonus) - salary_tax;
-                printf("Your Net Salary is :  %lfRiel\n",last_salary);
-	            printf("Your Net Salary is : %lf ",last_salary/exrate);
-        }
-    }
+                   last_salary = (salary + salary_notax + bonus) - salary_tax;
+                   printf("= Your Net Salary is :  %lfRiel \n",last_salary);
+                   printf("= Your Net Salary is : %lf \n",last_salary/exrate);
+                   printf("=========================================================");
+    return 0;
+               }
+}
 
 
